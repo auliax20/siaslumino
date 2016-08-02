87c5028024d611c21885c67a9a5406a46961da71
auliax20 <auliaulkhairi@gmail.com>
2016-08-02 17:23:39 +0700

    Alpha 0.1.9

--- yankee/app/Http/Controllers/Pustakacontroller.php
+++ yankee/app/Http/Controllers/Pustakacontroller.php
@@ -16,6 +16,38 @@ use Redirect;
 class Pustakacontroller extends Controller
 {
 	public function add(){
-		
+		$abuku = explode(" ",Input::get('kode_buku'));
+		$kbuku = $abuku[0];
+		$jbuku = Buku::where('kode_buku',$kbuku)->first();
+		$jpinj = Pustaka::where('kode_buku',$kbuku)->get()->count();
+		if($jpinj < $jbuku){
+			//add query
+		}else{
+			return redirect()->back()->with('error','Buku Kosong');	
+		}
 	}
+	public function viewPinjaman(){
+		$pustaka = Pustaka::orderBy('tanggal_batas','DESC')->get();
+		return redirect()->to('pustaka.viewpustaka')->with('pustaka',$pustaka);	
+	}
+	public function viewPinjamanLewat(){
+		$now = date(Y-m-d);
+		$pustaka = Pustaka::where('tanggal_batas','<',$now)->get();
+		return redirect()->to('pustaka.viewpustaka')->with('pustaka',$pustaka);	
+	}
+	public function viewPinjamanSiswa($nis){
+		$pustaka = Pustaka::where('nis',$nis)->get();
+		return redirect()->to('pustaka.viewpustaka')->with('pustaka',$pustaka);	
+	}
+	public function viewPinjamanBuku($buku){
+		$pustaka = Pustaka::where('kode_buku',$buku)->get();
+		return redirect()->to('pustaka.viewpustaka')->with('pustaka',$pustaka);		
+	}
+	public function kembaliBuku($idpinjam){
+		$now = date(Y-m-d);
+		$pustaka = Pustaka::where('id_pustaka', $idpinjam)->first();
+		$pustaka->delete();
+		return redirect()->to('pustaka.viewpustaka')->with('message','Buku berhasil dikembalikan');		
+	}
+	
 }
